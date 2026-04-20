8e43634928c310238d2da8dbebb73985bc6ea5d0
Andriamazotomahefa Manantsoa <156529932+mmana261@users.noreply.github.com>
2026-04-21 06:10:31 +0900

    update socials component

--- components/social_media.tsx
+++ components/social_media.tsx
@@ -1,30 +1,44 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
+
+const socials = [
+  {
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/mmana261",
+    image: "/linkedin.png",
+  },
+  {
+    title: "Facebook",
+    link: "https://www.facebook.com/manantsoa261",
+    image: "/facebook.png",
+  },
+  {
+    title: "Email",
+    link: "mailto:manamanantsoa@gmail.com",
+    image: "email.png",
+
+  },
+  
+
+]
+
 export default function SocialMedia() {
     return (
       <div className="flex flex-row items-center gap-2">
+        {socials.map((social, index) => (
         <a
-          href="https://www.linkedin.com/in/mmana261"
+          key={index}
+          href={social.link}
           target="_blank"
           rel="noopener noreferrer"
           className="w-7 h-7 flex items-center rounded-full overflow-hidden justify-center bg-transparent"
         >
           <img
-            src="/linkedin.png"
-            alt="LinkedIn"
+            src={social.image}
+            alt={social.title}
             className="w-7 h-7 scale-120"
           />
         </a>
-        <a
-          href="https://www.facebook.com/manantsoa261"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-7 h-7 flex items-center rounded-full overflow-hidden justify-center"
-        >
-          <img
-            src="/facebook.png"
-            alt="Facebook"
-            className="w-7 h-7"
-          />
-        </a>
+        ))}
         </div>
 
     );
